1917538edaec5822533b5a36810e94c4cfef7249
yongwookLee <yongw00k@g.skku.edu>
2022-05-07 06:13:00 +0900

    :tada: Create template.cpp

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Competitive-Programming
+# Competitive Programming
+
+## Template
+
+> [template.cpp](template.cpp)
+
+제 입맛대로 구성한 빠른 코딩을 위한 템플릿입니다. 꾸준히 업데이트될 예정입니다.
+
+### User Guide
+
+> Chrome에서도 똑같이 설정할 수 있지만 Firefox에서 하면 VS Code 위에서 바로 제출할 수 있습니다.
+
+- Firefox에 [Competitive Companion](https://addons.mozilla.org/ko/firefox/addon/competitive-companion/) 애드온을 설치합니다.
+- VS Code에서 [Competitive Programming Helper](https://marketplace.visualstudio.com/items?itemName=DivyanshuAgrawal.competitive-programming-helper) 플러그인을 설치합니다.
+- VS Code에서 `⌘ ,`를 누르고 cph를 검색합니다.
+- `Cph › General: Default Language` 탭에서 원하는 default 언어를 설정합니다.
+- `Cph › General: Default Language Template File Location` 탭에서 template 코드의 위치를 넣어줍니다.
+  > Ex) `/Users/yongwooklee/Github/icpc/note/template.cpp`
+- 기타 컴파일 옵션과 같은 설정들을 완료합니다.
---
+++ template.cpp
@@ -0,0 +1,44 @@
+/**
+ *    author:  i4song
+**/
+#include <bits/stdc++.h>
+
+#define fv(x,v) for(auto& x: v)
+#define fn(i,n) for(int i=0;i<n;++i)
+#define fnr(i,n) for(int i=n-1;i>=0;--i)
+#define fab(i,a,b) for(int i=a;i<b;++i)
+#define cv(v) for(auto& x: v) cin >> x
+#define pb(x) push_back(x)
+#define sz(v) int(v.size())
+#define no "NO"
+#define yes "YES"
+#define X first
+#define Y second
+#define endl '\n'
+#define PI acos(-1)
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vc = vector<char>;
+
+
+void solve() {
+  
+  return;
+}
+
+int main() {
+  cin.tie(0) -> sync_with_stdio(0);
+
+  int tt = 1;
+  cin >> tt;
+  while (tt--){
+    solve();
+  }
+}
