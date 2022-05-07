c105a15960a493d3ecd19b9e0b0c6ac2441204ee
yongwookLee <yongw00k@g.skku.edu>
2022-05-07 09:26:08 +0900

    :recycle: Make functions global

--- Data Structure/FenwickTree.cpp
+++ Data Structure/FenwickTree.cpp
@@ -6,13 +6,15 @@ using namespace std;
 using ll = long long;
 using vll = vector<ll>;
 
-void solve() {
-  ll n;
+int max_n;
 
-  // Fenwick Tree
-  vll v(n+1);
-  vll tree(n+1);
-  auto query = [&](ll i) {ll ret = 0; for(; i; i -= i&-i) ret += tree[i]; return ret;};
-  auto update = [&](ll i,ll k) {for(; i<sz(tree); i += i&-i) tree[i] += k;};
-
-}
+vll v(max_n);
+vll tree(max_n);
+ll query (ll i) {
+  ll ret = 0;
+  for(; i; i -= i&-i) ret += tree[i];
+  return ret;
+};
+void update(ll i,ll k) {
+  for(; i<sz(tree); i += i&-i) tree[i] += k;
+};
