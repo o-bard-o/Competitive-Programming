b20a651dbe7f9659f16e5ffcb257acbeb039ed0f
yongwookLee <yongw00k@g.skku.edu>
2022-05-07 09:02:53 +0900

    :sparkles: Add fba macro

--- template.cpp
+++ template.cpp
@@ -1,12 +1,9 @@
-/**
- *    author:  i4song
-**/
 #include <bits/stdc++.h>
-
 #define fv(x,v) for(auto& x: v)
 #define fn(i,n) for(int i=0;i<n;++i)
 #define fnr(i,n) for(int i=n-1;i>=0;--i)
-#define fab(i,a,b) for(int i=a;i<b;++i)
+#define fab(i,a,b) for(int i=a;i<=b;++i)
+#define fba(i,b,a) for(int i=b;i>=a;--i)
 #define cv(v) for(auto& x: v) cin >> x
 #define pb(x) push_back(x)
 #define sz(v) int(v.size())
@@ -16,17 +13,14 @@
 #define Y second
 #define endl '\n'
 #define PI acos(-1)
-
 #pragma GCC optimize("O3")
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("unroll-loops")
-
 using namespace std;
 using ll = long long;
 using pll = pair<ll, ll>;
 using vll = vector<ll>;
 using vc = vector<char>;
-
 
 void solve() {
   
